f3699e95eaf757aec646cc04a52e70020da60ea1
anshuparjapati <anshuparjapati3@gmail.com>
2019-12-23 13:51:43 +0530

    latest update

--- app/src/main/java/com/cytrucksindia/cytracking/Main4Activity.java
+++ app/src/main/java/com/cytrucksindia/cytracking/Main4Activity.java
@@ -1,37 +1,14 @@
 package com.cytrucksindia.cytracking;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
-
 public class Main4Activity extends AppCompatActivity {
-    EditText edit1;
-    Button button1;
-    private Object Main5Activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
-        edit1= (EditText) findViewById(R.id.edit1);
-        button1 = (Button) findViewById(R.id.button1);
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String namevalue= edit1.getText().toString();
-                Intent intent = new Intent (Main4Activity.this,Main5Activity.class);
-                intent.putExtra(namevalue,"namevalue");
-                startActivity(intent);
-            }
-
-
-        });
     }
 }
--- app/src/main/java/com/cytrucksindia/cytracking/MainActivity.java
+++ app/src/main/java/com/cytrucksindia/cytracking/MainActivity.java
@@ -1,27 +1,68 @@
 package com.cytrucksindia.cytracking;
 
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-
 public class MainActivity extends AppCompatActivity {
+    EditText edit;
+    ImageView abcd, clover;
+    LinearLayout textsplash, texthome, menus;
+    Animation frombottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int SPLASH_TIME_OUT = 5000;
-        new Handler().postDelayed(new Runnable() {
+
+        frombottom = AnimationUtils.loadAnimation(this, R.anim.frombottom);
+
+
+        abcd = (ImageView) findViewById(R.id.abcd);
+        clover = (ImageView) findViewById(R.id.clover);
+        textsplash = (LinearLayout) findViewById(R.id.textsplash);
+        texthome = (LinearLayout) findViewById(R.id.texthome);
+        menus = (LinearLayout) findViewById(R.id.menus);
+
+        abcd.animate().translationY(-1700).setDuration(2500).setStartDelay(400);
+        clover.animate().alpha(0).setDuration(800).setStartDelay(600);
+        textsplash.animate().translationY(140).alpha(0).setDuration(800).setStartDelay(300);
+
+        texthome.startAnimation(frombottom);
+        menus.startAnimation(frombottom);
+
+
+
+
+        edit = findViewById(R.id.edit);
+        findViewById(R.id.button).setOnClickListener(   new View.OnClickListener() {
             @Override
-            public void run() {
-                Intent homeIntent = new Intent(MainActivity.this,Main2Activity.class);
-                startActivity(homeIntent);
-                finish();
+            public void onClick(View v) {
+
+                String mobile = edit.getText().toString().trim();
+
+                if(mobile.isEmpty() || mobile.length() < 10){
+                    edit.setError("Enter a valid mobile");
+                    edit.requestFocus();
+                    return;
+                }
+
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                intent.putExtra("mobile", mobile);
+                startActivity(intent);
             }
-        }, SPLASH_TIME_OUT);
+        });
+
     }
 }
+
+
