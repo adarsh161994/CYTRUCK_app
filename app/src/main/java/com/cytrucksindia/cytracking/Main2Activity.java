ce172c6114010a07c8b8a3180b4e8f945bd02d3a
adarsh1694 <adarsh@longeo.in>
2020-01-30 09:33:16 +0530

    new apk

--- app/src/main/java/com/cytrucksindia/cytracking/Main2Activity.java
+++ app/src/main/java/com/cytrucksindia/cytracking/Main2Activity.java
@@ -27,6 +27,8 @@ import java.util.concurrent.TimeUnit;
 
 public class Main2Activity  extends AppCompatActivity {
 
+
+
     //These are the objects needed
     //It is the verification id that will be sent to the user
     private String mVerificationId;
